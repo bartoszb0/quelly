de173636d22d2f1664d509744698d9449bb7ff2d
bartoszb0 <bartoszb2020@gmail.com>
2026-06-23 23:18:02 +0200

    import shopsModule

--- backend/src/shifts/shifts.module.ts
+++ backend/src/shifts/shifts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
-import { ShiftsService } from './shifts.service';
+import { ShopsModule } from '../shops/shops.module';
 import { ShiftsController } from './shifts.controller';
+import { ShiftsService } from './shifts.service';
 
 @Module({
+  imports: [ShopsModule],
   controllers: [ShiftsController],
   providers: [ShiftsService],
 })
