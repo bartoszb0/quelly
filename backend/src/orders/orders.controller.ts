24628a5ae5eef4f14d463470dceafb051576f5d0
bartoszb0 <bartoszb2020@gmail.com>
2026-06-24 15:54:31 +0200

    feat: enhance OrdersController and OrdersService to support shop-specific order creation with validation and creating numbers

--- backend/src/orders/orders.controller.ts
+++ backend/src/orders/orders.controller.ts
@@ -1,15 +1,34 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { OrdersService } from './orders.service';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import type { UUID } from 'crypto';
+import { JwtGuard } from '../auth/guards/jwt.guard';
+import { CurrentUser } from '../common/decorators/current-user.decorator';
+import type { UserPayload } from '../common/types/user-payload.type';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
+import { OrdersService } from './orders.service';
 
-@Controller('orders')
+@Controller('/shops/:shopId/orders')
+@UseGuards(JwtGuard)
 export class OrdersController {
   constructor(private readonly ordersService: OrdersService) {}
 
   @Post()
-  create(@Body() createOrderDto: CreateOrderDto) {
-    return this.ordersService.create(createOrderDto);
+  create(
+    @Param('shopId', ParseUUIDPipe) shopId: UUID,
+    @Body() createOrderDto: CreateOrderDto,
+    @CurrentUser() user: UserPayload,
+  ) {
+    return this.ordersService.create(shopId, createOrderDto, user.id);
   }
 
   @Get()
--- backend/src/orders/orders.service.ts
+++ backend/src/orders/orders.service.ts
@@ -1,11 +1,70 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { ShiftsService } from '../shifts/shifts.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 import { UpdateOrderDto } from './dto/update-order.dto';
 
 @Injectable()
 export class OrdersService {
-  create(createOrderDto: CreateOrderDto) {
-    return 'This action adds a new order';
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly shiftsService: ShiftsService,
+  ) {}
+
+  private async getNextNumber(shiftId: string) {
+    const latestOrder = await this.prisma.order.findFirst({
+      where: {
+        shiftId: shiftId,
+      },
+      orderBy: { number: 'desc' },
+    });
+
+    return !latestOrder ? 1 : latestOrder.number + 1;
+  }
+
+  async create(shopId: string, createOrderDto: CreateOrderDto, userId: string) {
+    const shift = await this.shiftsService.getActiveOrThrow(shopId, userId);
+
+    let orderItems: {
+      menuItemId: string;
+      quantity: number;
+      nameSnapshot: string;
+    }[] = [];
+
+    if (createOrderDto.items) {
+      const itemIds = createOrderDto.items.map((i) => i.menuItemId);
+
+      const menuItems = await this.prisma.menuItem.findMany({
+        where: { id: { in: itemIds }, shopId },
+      });
+
+      if (menuItems.length !== new Set(itemIds).size)
+        throw new BadRequestException('One or more menu items are invalid');
+
+      orderItems = createOrderDto.items.map((dtoItem) => {
+        const menuItem = menuItems.find((mi) => mi.id === dtoItem.menuItemId);
+
+        return {
+          menuItemId: dtoItem.menuItemId,
+          quantity: dtoItem.quantity,
+          nameSnapshot: menuItem!.name,
+        };
+      });
+    }
+
+    const number = await this.getNextNumber(shift.id);
+
+    return this.prisma.order.create({
+      data: {
+        shopId: shopId,
+        shiftId: shift.id,
+        number: number,
+        items: {
+          create: orderItems,
+        },
+      },
+      include: { items: true },
+    });
   }
 
   findAll() {
