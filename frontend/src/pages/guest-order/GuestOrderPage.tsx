793a75525c79630298267790e7d4430874a16616
bartoszb0 <bartoszb2020@gmail.com>
2026-06-25 19:27:46 +0200

    feat: add realtime connection

--- frontend/src/pages/guest-order/GuestOrderPage.tsx
+++ frontend/src/pages/guest-order/GuestOrderPage.tsx
@@ -1,27 +1,34 @@
 import { getOrder } from "@/api/public";
 import LoadingSpinner from "@/components/common/LoadingSpinner";
 import { QueryError } from "@/components/common/QueryError";
+import { useOrderSocket } from "@/hooks/useOrderSocket";
 import { isValidUuid } from "@/lib/validation";
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { Navigate, useParams } from "react-router-dom";
 
 export default function GuestOrderPage() {
   const { shopPublicId, orderNumber } = useParams();
+  const queryClient = useQueryClient();
 
-  if (!orderNumber || !shopPublicId || !isValidUuid(shopPublicId)) {
-    return <Navigate to="/not-found" replace />;
-  }
+  const valid = !!orderNumber && !!shopPublicId && isValidUuid(shopPublicId);
 
   const { isPending, error, data } = useQuery({
     queryKey: ["orderPage", shopPublicId, orderNumber],
-    queryFn: () => getOrder(shopPublicId, orderNumber),
+    queryFn: () => getOrder(shopPublicId!, orderNumber!),
+    enabled: valid,
   });
+
+  useOrderSocket(shopPublicId, valid, () => {
+    queryClient.invalidateQueries({
+      queryKey: ["orderPage", shopPublicId, orderNumber],
+    });
+  });
+
+  if (!valid) return <Navigate to="/not-found" replace />;
 
   if (isPending) return <LoadingSpinner />;
 
   if (error) return <QueryError error={error} />;
-
-  console.log(data);
 
   return (
     <div>
@@ -33,12 +40,10 @@ export default function GuestOrderPage() {
       <h1>Placed at: {data.createdAt}</h1>
 
       <div className="flex flex-col mt-10">
-        {data.items.map((item) => (
-          <div>
-            <h2>
-              {item.nameSnapshot} - x{item.quantity}
-            </h2>
-          </div>
+        {data.items.map((item, index) => (
+          <h2 key={index}>
+            {item.nameSnapshot} - x{item.quantity}
+          </h2>
         ))}
       </div>
     </div>
