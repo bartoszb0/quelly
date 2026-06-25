800cd6a0a00c3a458b86ffccff9f8b20e690975a
bartoszb0 <bartoszb2020@gmail.com>
2026-06-25 14:50:14 +0200

    add sonner

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { Toaster } from "sonner";
 import App from "./App.tsx";
 import "./index.css";
 
@@ -12,6 +13,7 @@ createRoot(document.getElementById("root")!).render(
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <App />
+        <Toaster position="bottom-center" />
       </BrowserRouter>
     </QueryClientProvider>
   </StrictMode>,
