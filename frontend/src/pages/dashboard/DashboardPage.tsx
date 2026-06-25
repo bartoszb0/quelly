9dbc410ebdbf49817ea978a6cd8e82eff09c21b2
bartoszb0 <bartoszb2020@gmail.com>
2026-06-26 00:42:20 +0200

    extract logout btn into separate component

--- frontend/src/pages/dashboard/DashboardPage.tsx
+++ frontend/src/pages/dashboard/DashboardPage.tsx
@@ -1,33 +1,9 @@
-import { signOut } from "@/api/auth";
-import { Button } from "@/components/ui/button";
-import { toastApiError } from "@/lib/toastApiError";
-import { useQueryClient } from "@tanstack/react-query";
-import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import LogoutBtn from "@/components/common/LogoutBtn";
 
 export default function DashboardPage() {
-  const navigate = useNavigate();
-  const [isLoading, setLoading] = useState(false);
-  const queryClient = useQueryClient();
-
-  const handleLogout = async () => {
-    setLoading(true);
-    try {
-      await signOut();
-      queryClient.clear();
-      navigate("/login", { replace: true });
-    } catch (e) {
-      toastApiError(e);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
     <div>
-      <Button onClick={handleLogout} disabled={isLoading}>
-        Logout
-      </Button>
+      <LogoutBtn />
     </div>
   );
 }
