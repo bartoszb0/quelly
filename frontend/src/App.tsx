02aacb653108e7342eedee27fa96223dacc92071
bartoszb0 <bartoszb2020@gmail.com>
2026-06-25 15:50:48 +0200

    add basic routes and NotFoundPage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,27 @@
 import { Route, Routes } from "react-router-dom";
+import GuestOrderPage from "./pages/GuestOrderPage";
+import GuestShopPage from "./pages/GuestShopPage";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
+import NotFoundPage from "./pages/NotFoundPage";
 import RegisterPage from "./pages/RegisterPage";
 
 function App() {
   return (
     <Routes>
+      {/* Auth and homepage */}
       <Route path="/" element={<HomePage />} />
       <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
+
+      {/* Guarded pages */}
+      {/* <Router path='/shop'/> */}
+
+      {/* Public pages */}
+      <Route path="/s/:shopPublicId" element={<GuestShopPage />} />
+      <Route path="/s/:shopPublicId/:number" element={<GuestOrderPage />} />
+
+      <Route path="*" element={<NotFoundPage />} />
     </Routes>
   );
 }
