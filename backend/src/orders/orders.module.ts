5a684c5d18585ebc8e9e195dbd8cf0d756847024
bartoszb0 <bartoszb2020@gmail.com>
2026-06-25 19:23:11 +0200

    feat: add RealtimeModule to OrdersModule imports

--- backend/src/orders/orders.module.ts
+++ backend/src/orders/orders.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { RealtimeModule } from '../realtime/realtime.module';
 import { ShiftsModule } from '../shifts/shifts.module';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
 
 @Module({
-  imports: [ShiftsModule],
+  imports: [ShiftsModule, RealtimeModule],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
