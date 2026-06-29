90db1ba9d35d5789b39720c7c3d5c5c3945edde4
bartoszb0 <bartoszb2020@gmail.com>
2026-06-29 15:15:35 +0200

    add redirectOn404 prop to QueryError component

--- frontend/src/components/common/QueryError.tsx
+++ frontend/src/components/common/QueryError.tsx
@@ -1,8 +1,18 @@
 import { AxiosError } from "axios";
 import { Navigate } from "react-router-dom";
 
-export function QueryError({ error }: { error: unknown }) {
-  if (error instanceof AxiosError && error.response?.status === 404) {
+export function QueryError({
+  error,
+  redirectOn404 = false,
+}: {
+  error: unknown;
+  redirectOn404?: boolean;
+}) {
+  if (
+    redirectOn404 &&
+    error instanceof AxiosError &&
+    error.response?.status === 404
+  ) {
     return <Navigate to="/not-found" replace />;
   }
   return <div>Something went wrong. Please try again.</div>;
