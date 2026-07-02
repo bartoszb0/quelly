0aba889f0bf248c24f5cd68d97b1b708092298f8
bartoszb0 <bartoszb2020@gmail.com>
2026-07-02 13:36:04 +0200

    refactor MenuItems component to use MenuItem for rendering menu items

--- frontend/src/pages/dashboard/menu/components/MenuItems.tsx
+++ frontend/src/pages/dashboard/menu/components/MenuItems.tsx
@@ -1,8 +1,8 @@
 import { getMenuItems } from "@/api/menu";
 import LoadingSpinner from "@/components/common/LoadingSpinner";
 import { QueryError } from "@/components/common/QueryError";
-import { Card } from "@/components/ui/card";
 import { useQuery } from "@tanstack/react-query";
+import MenuItem from "./MenuItem";
 
 export default function MenuItems({ shopId }: { shopId: string }) {
   const { isPending, error, data, refetch } = useQuery({
@@ -26,14 +26,9 @@ export default function MenuItems({ shopId }: { shopId: string }) {
             {data.length} {data.length === 1 ? "item" : "items"} on your menu
           </p>
           <div className="mt-4 grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
-          {data.map((item) => (
-            <Card
-              key={item.id}
-              className="flex aspect-square items-center justify-center p-4 text-center"
-            >
-              <span className="font-medium wrap-break-word">{item.name}</span>
-            </Card>
-          ))}
+            {data.map((item) => (
+              <MenuItem key={item.id} item={item} />
+            ))}
           </div>
         </>
       )}
