31424d82b80bb196abb1d52f7d6f9ceac7250e9a
bartoszb0 <bartoszb2020@gmail.com>
2026-07-02 14:30:25 +0200

    add new shift types

--- frontend/src/types/Shift.ts
+++ frontend/src/types/Shift.ts
@@ -1,6 +1,16 @@
+import type { Order } from "./Order";
+
 export type Shift = {
   id: string;
   shopId: string;
   startedAt: string;
-  endedAt: string;
+  endedAt: string | null;
+};
+
+export type ShiftListItem = Shift & {
+  ordersCount: number;
+};
+
+export type ShiftDetail = Shift & {
+  orders: Order[];
 };
