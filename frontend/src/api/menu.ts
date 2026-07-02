792227bf2940c187577c91905b2044a6b626143e
bartoszb0 <bartoszb2020@gmail.com>
2026-07-02 12:53:09 +0200

    add api call to create Menu item

--- frontend/src/api/menu.ts
+++ frontend/src/api/menu.ts
@@ -1,6 +1,16 @@
+import type { MenuItemInput } from "@/schemas/menuItemSchema";
+import type { MenuItem } from "@/types/MenuItem";
 import { api } from "./client";
 
-export const getMenuItems = async (shopId: string) => {
-  const res = await api.get(`/shops/${shopId}/menu-items`);
+export const getMenuItems = async (shopId: string): Promise<MenuItem[]> => {
+  const res = await api.get<MenuItem[]>(`/shops/${shopId}/menu-items`);
+  return res.data;
+};
+
+export const createMenuItem = async (
+  shopId: string,
+  body: MenuItemInput,
+): Promise<MenuItem> => {
+  const res = await api.post<MenuItem>(`/shops/${shopId}/menu-items`, body);
   return res.data;
 };
