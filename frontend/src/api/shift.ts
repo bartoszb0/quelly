d3740fe94ab61c066ccc092fb208215bef94c393
bartoszb0 <bartoszb2020@gmail.com>
2026-07-02 14:30:46 +0200

    update api calls with updated types

--- frontend/src/api/shift.ts
+++ frontend/src/api/shift.ts
@@ -1,12 +1,17 @@
-import type { Shift } from "@/types/Shift";
+import type { ShiftDetail, ShiftListItem } from "@/types/Shift";
 import { api } from "./client";
 
-export const getShifts = async (shopId: string): Promise<Shift[]> => {
-  const res = await api.get<Shift[]>(`/shops/${shopId}/shifts`);
+export const getShifts = async (
+  shopId: string,
+): Promise<ShiftListItem[]> => {
+  const res = await api.get<ShiftListItem[]>(`/shops/${shopId}/shifts`);
   return res.data;
 };
 
-export const getShift = async (shopId: string, shiftId: string) => {
-  const res = await api.get(`shops/${shopId}/shifts/${shiftId}`);
+export const getShift = async (
+  shopId: string,
+  shiftId: string,
+): Promise<ShiftDetail> => {
+  const res = await api.get<ShiftDetail>(`/shops/${shopId}/shifts/${shiftId}`);
   return res.data;
 };
