73330cfe42189c2f7d6751a6c4527b810b7aa40d
bartoszb0 <bartoszb2020@gmail.com>
2026-07-06 18:32:28 +0200

    feat: update ShopOpen component to accept shift prop and handle shift change

--- frontend/src/pages/dashboard/shop/components/ShopOpen.tsx
+++ frontend/src/pages/dashboard/shop/components/ShopOpen.tsx
@@ -1,7 +1,16 @@
-export default function ShopOpen() {
+import { Button } from "@/components/ui/button";
+import type { ActiveShift } from "@/types/Shift";
+
+export default function ShopOpen({ shift }: { shift: ActiveShift }) {
+  const handleShiftChange = () => {
+    console.log("change");
+  };
+
   return (
     <div className="mx-auto max-w-3xl px-6 py-8">
       <h1>todo</h1>
+      <Button onClick={handleShiftChange}>End shift</Button>
+      <p>{shift.id}</p>
     </div>
   );
 }
