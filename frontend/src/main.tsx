1367fcc500cc836e0b1e6d4ec7ec524fde66ff21
bartoszb0 <bartoszb2020@gmail.com>
2026-07-07 19:11:53 +0200

    feat: add i18n support with i18next and language detector

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,6 +5,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "sonner";
 import App from "./App.tsx";
+import "./i18n";
 import "./index.css";
 
 const queryClient = new QueryClient({
